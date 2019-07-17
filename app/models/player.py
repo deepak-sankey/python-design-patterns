ece599e78f242c1426cd49f4607f088ff041074a
Digambar Gadekar <digambar.g@sankeysolutions.com>
2019-07-17 23:27:45 +0530

    Divide Code in Module

--- app/models/player.py
+++ app/models/player.py
@@ -1,14 +1,62 @@
-class Bowler:
+from flask import render_template
+from flask import request, jsonify
+import json
+from app import app 
+import os
+
+def read_json_data(data_source):
+   fp = open(data_source, encoding="utf8")
+   data = json.loads(fp.read())
+   return data
+
+#Class Definitions
+#app/models/player.py
+class Player:
     id = ""
-    playerId = ""
-    matches = 0
-    innings = 0
-    runs = 0
-    balls = 0
-    wickets = 0
-    maidens = 0
-    
-    # def __init__(self):
+    name = ""
+    role = ""
+    battingStyle = ""
+    bowlingStyle = ""
+    country = ""
+    battingInfo = {}
+    bowlingInfo = {}
+    isDeleted = False
+
+    def __init__(self, player):
+        # print(type(player)")
+        self.id = player["id"]
+        self.name = player["name"]
+        self.role = player["role"]
+        self.battingStyle = player["battingStyle"]
+        self.bowlingStyle = player["bowlingStyle"]
+        self.country = player["country"]
+        
+        batsmenInfoArray = read_json_data(os.path.join(os.path.dirname(os.path.realpath(__file__)), "../database/batsman.json"))
+        bowlerInfoArray = read_json_data(os.path.join(os.path.dirname(os.path.realpath(__file__)), "../database/bowler.json"))
+
+        
+        if self.role == "Batsman" or self.role == "WK-Batsman" or self.role == "Batting Allrounder" or self.role == "Bowling Allrounder":
+            battingInfo = list(filter(lambda item: self.id == item["playerId"], batsmenInfoArray))[0];
+            self.battingInfo = {
+                "id" : battingInfo["id"],
+                "playerId" : battingInfo["playerId"],
+                "matches" : battingInfo["matches"],
+                "innings" : battingInfo["innings"],
+                "runs" : battingInfo["runs"],
+                "balls" : battingInfo["balls"]
+            }
+        else:
+            bowlingInfo = list(filter(lambda item: self.id == item["playerId"], bowlerInfoArray))[0];
+            self.bowlingInfo = {
+                "id" : bowlingInfo["id"],
+                "playerId" : bowlingInfo["playerId"],
+                "matches" : bowlingInfo["matches"],
+                "innings" : bowlingInfo["innings"],
+                "runs" : bowlingInfo["runs"],
+                "balls" : bowlingInfo["balls"],
+                "wickets" : bowlingInfo["wickets"]
+            }
+
         
 
 
